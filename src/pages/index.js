9bb93ed97fc6a26bc8326783820d3ca286b906b9
jon <jcoleman1388@gmail.com>
2022-01-02 15:36:01 -0500

    typo on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,15 @@ export default function Index() {
         <h1>Hello, I'm Jon</h1>
         <p>
           I am a technical writer and web designer here at the internet. I'm
-          interested in documentation and writing for the web, front end
+          interested in documentation and writing for the web, frontend
           development, web design, and accessibility. I also collect punk
           records and try to get outside, preferably on a bike or a trail, as
           often as I can.{' '}
         </p>
         <p>
           If you're interested, you can read more{' '}
-          <Link to="/about">about me</Link>, see some of my projects{' '}
-          <Link to="/projects">right here</Link>, and read{' '}
+          <Link to="/about">about me</Link>, see some of my{' '}
+          <Link to="/projects">projects</Link>, and read{' '}
           <Link to="/writing">my thoughts</Link> on all kinds of things.
         </p>
       </article>
