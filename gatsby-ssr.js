661f0676c85b77914e291249278f06b68667c0dd
jon <jcoleman1388@gmail.com>
2022-01-04 17:52:18 -0500

    working on FOUC issues

---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Layout from './src/components/Layout';
+
+export function wrapPageElement({ element, props }) {
+  return <Layout {...props}>{element}</Layout>;
+}
