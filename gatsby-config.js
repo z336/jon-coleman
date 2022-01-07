838e6d64df67e58e0dc0bca0b78dbb28f035342d
jon <jcoleman1388@gmail.com>
2022-01-07 17:10:04 -0500

    adding rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,27 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://joncoleman.me',
-    title: 'Jon Coleman',
+    siteUrl: `https://joncoleman.me`,
+    title: `Jon Coleman`,
+    description: `I am a technical writer and web designer here at the internet. I'm interested in documentation and writing for the web, front-end web development, web design, and accessibility.`,
   },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-preload-fonts',
-    'gatsby-plugin-image',
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-preload-fonts`,
+    `gatsby-plugin-image`,
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: `gatsby-plugin-react-svg`,
       options: {
         rule: {
           include: /assets/,
         },
       },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: 'src/assets/images/icon.png',
+        icon: `src/assets/images/icon.png`,
       },
     },
     {
@@ -36,8 +37,8 @@ module.exports = {
         ],
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -51,6 +52,63 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/content/`,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
+            }
+          `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.description,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+                {
+                  allMdx(
+                    filter: {frontmatter: {category: {eq: "writing"}}}
+                    sort: {fields: frontmatter___date, order: DESC}
+                  ) {
+                    edges {
+                      node {
+                        id
+                        frontmatter {
+                          title
+                          date(formatString: "MMMM Do, YYYY")
+                          description
+                        }
+                        fields {
+                          slug
+                        }
+                      }
+                    }
+                  }
+                }
+              `,
+            output: '/rss.xml',
+            title: 'Jon Coleman RSS Feed',
+          },
+        ],
       },
     },
   ],
