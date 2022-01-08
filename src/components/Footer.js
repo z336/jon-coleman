695df92309858563feaf497f00998e06bb4db476
jon <jcoleman1388@gmail.com>
2022-01-08 13:32:07 -0500

    added RSS link and working on disabling hover on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import * as styles from './Footer.module.scss';
+import { ImRss } from 'react-icons/im';
 
 export default function Footer() {
   return (
     <footer>
+      <div className={styles.rss}>
+        <a href="/rss.xml">
+          <ImRss /> RSS
+        </a>
+      </div>
       <div>
-        <small>not © {new Date().getFullYear()}</small>
+        <small>Jon Coleman made all this stuff.</small>
       </div>
     </footer>
   );
